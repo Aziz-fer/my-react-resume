d2b8bc598cad69beb764e47ccb3b775b109aa296
Aziz-fer <ferianiaziz@gmail.com>
2024-04-24 02:01:40 +0200

    customized profile

--- src/data/data.tsx
+++ src/data/data.tsx
@@ -20,11 +20,6 @@ import porfolioImage3 from '../images/portfolio/portfolio-3.jpg';
 import porfolioImage4 from '../images/portfolio/portfolio-4.jpg';
 import porfolioImage5 from '../images/portfolio/portfolio-5.jpg';
 import porfolioImage6 from '../images/portfolio/portfolio-6.jpg';
-import porfolioImage7 from '../images/portfolio/portfolio-7.jpg';
-import porfolioImage8 from '../images/portfolio/portfolio-8.jpg';
-import porfolioImage9 from '../images/portfolio/portfolio-9.jpg';
-import porfolioImage10 from '../images/portfolio/portfolio-10.jpg';
-import porfolioImage11 from '../images/portfolio/portfolio-11.jpg';
 import profilepic from '../images/profilepic.jpg';
 import testimonialImage from '../images/testimonial.webp';
 import {
@@ -73,14 +68,14 @@ export const heroData: Hero = {
   description: (
     <>
       <p className="prose-sm text-stone-200 sm:prose-base lg:prose-lg">
-        I'm a Victoria based <strong className="text-stone-100">Full Pix Software Engineer</strong>, currently working
-        at <strong className="text-stone-100">Instant Domains</strong> helping build a modern, mobile-first, domain
-        registrar and site builder.
+        I'm a Tunisian Tu Dortmund Computer Science graduate , worked
+        at <strong className="text-stone-100">WILO</strong> as a Software Testing helping build Python tools for Testing 
+        and Testing different software releases.
       </p>
       <p className="prose-sm text-stone-200 sm:prose-base lg:prose-lg">
-        In my free time time, you can catch me training in <strong className="text-stone-100">Muay Thai</strong>,
-        plucking my <strong className="text-stone-100">banjo</strong>, or exploring beautiful{' '}
-        <strong className="text-stone-100">Vancouver Island</strong>.
+        In my free time time, you can catch me training in <strong className="text-stone-100">All inklusive</strong>,
+        or coding and learning new techs such as <strong className="text-stone-100">Cloud AWS</strong>, or exploring beautiful{' '}
+        <strong className="text-stone-100">Places</strong>.
       </p>
     </>
   ),
@@ -104,16 +99,14 @@ export const heroData: Hero = {
  */
 export const aboutData: About = {
   profileImageSrc: profilepic,
-  description: `Use this bio section as your way of describing yourself and saying what you do, what technologies you like
-  to use or feel most comfortable with, describing your personality, or whatever else you feel like throwing
-  in.`,
+  description: `Software Tester with 2 years of experience as a working student with good experience using Python, Jenkins
+  Git, Bash and multiple other tools`,
   aboutItems: [
-    {label: 'Location', text: 'Victoria, BC', Icon: MapIcon},
-    {label: 'Age', text: '29', Icon: CalendarIcon},
-    {label: 'Nationality', text: 'Canadian / Irish', Icon: FlagIcon},
-    {label: 'Interests', text: 'Motorcycles, Muay Thai, Banjos', Icon: SparklesIcon},
-    {label: 'Study', text: 'University of Victoria', Icon: AcademicCapIcon},
-    {label: 'Employment', text: 'Instant Domains, inc.', Icon: BuildingOffice2Icon},
+    {label: 'Location', text: 'Dortmund, Germany', Icon: MapIcon},
+    {label: 'Age', text: '24', Icon: CalendarIcon},
+    {label: 'Nationality', text: 'Tunisian', Icon: FlagIcon},
+    {label: 'Interests', text: 'Gaming,Traveling,Fitness Training,Coding', Icon: SparklesIcon},
+    {label: 'Study', text: 'Technical University Of Dortmund', Icon: AcademicCapIcon},
   ],
 };
 
@@ -126,66 +119,114 @@ export const skills: SkillGroup[] = [
     skills: [
       {
         name: 'English',
-        level: 10,
+        level: 9,
       },
       {
         name: 'French',
-        level: 4,
-      },
-      {
-        name: 'Spanish',
-        level: 3,
-      },
-    ],
-  },
-  {
-    name: 'Frontend development',
-    skills: [
-      {
-        name: 'React',
-        level: 9,
-      },
-      {
-        name: 'Typescript',
-        level: 7,
-      },
-      {
-        name: 'GraphQL',
-        level: 6,
-      },
-    ],
-  },
-  {
-    name: 'Backend development',
-    skills: [
-      {
-        name: 'Node.js',
         level: 8,
       },
       {
-        name: 'Rust',
-        level: 5,
+        name: 'Deutsch',
+        level: 9,
       },
       {
-        name: 'Golang',
-        level: 4,
+        name: 'Arabic',
+        level: 10,
       },
     ],
   },
   {
-    name: 'Mobile development',
+    name: 'Software Testing',
     skills: [
       {
-        name: 'React Native',
+        name: 'Python',
+        level: 10,
+      },
+      {
+        name: 'Matlab',
+        level: 8,
+      },
+      {
+        name: 'NI TestStand/NI LabVIEW',
+        level: 7,
+      },
+      {
+        name: 'Test Automation',
+        level: 8,
+      },
+      {
+        name: 'Jenkins',
+        level: 5,
+      },
+      {
+        name: 'Tortoise SVN/Git',
         level: 9,
       },
       {
-        name: 'Flutter',
-        level: 4,
+        name: 'Polarion',
+        level: 8,
       },
       {
-        name: 'Swift',
-        level: 3,
+        name: 'Bash',
+        level: 9,
+      },
+    ],
+  },
+  {
+    name: 'Skills I learned at the University',
+    skills: [
+      {
+        name: 'Java',
+        level: 10,
+      },
+      {
+        name: 'Kotlin',
+        level: 7,
+      },
+      {
+        name: 'C',
+        level: 2,
+      },
+      {
+        name: 'Blockchain and Web3',
+        level: 9,
+      },
+      {
+        name: 'SQL',
+        level: 10,
+      },
+      {
+        name: 'R',
+        level: 9,
+      },
+    ],
+  },
+  {
+    name: 'Skills I learned alone',
+    skills: [
+      {
+        name: 'Amazon Web Services (AWS)',
+        level: 8,
+      },
+      {
+        name: 'Docker',
+        level: 5,
+      },
+      {
+        name: 'Game Development using Godot/Unreal Engine',
+        level: 9,
+      },
+      {
+        name: 'Terraform',
+        level: 6,
+      },
+      {
+        name: 'NoSQL',
+        level: 9,
+      },
+      {
+        name: 'Microsoft PowerBI',
+        level: 6,
       },
     ],
   },
@@ -198,68 +239,38 @@ export const portfolioItems: PortfolioItem[] = [
   {
     title: 'Project title 1',
     description: 'Give a short description of your project here.',
-    url: 'https://reactresume.com',
+    url: 'https://github.com/Aziz-fer',
     image: porfolioImage1,
   },
   {
     title: 'Project title 2',
     description: 'Give a short description of your project here.',
-    url: 'https://reactresume.com',
+    url: 'https://github.com/Aziz-fer',
     image: porfolioImage2,
   },
   {
     title: 'Project title 3',
     description: 'Give a short description of your project here.',
-    url: 'https://reactresume.com',
+    url: 'https://github.com/Aziz-fer',
     image: porfolioImage3,
   },
   {
     title: 'Project title 4',
     description: 'Give a short description of your project here.',
-    url: 'https://reactresume.com',
+    url: 'https://github.com/Aziz-fer',
     image: porfolioImage4,
   },
   {
     title: 'Project title 5',
     description: 'Give a short description of your project here.',
-    url: 'https://reactresume.com',
+    url: 'https://github.com/Aziz-fer',
     image: porfolioImage5,
   },
   {
     title: 'Project title 6',
     description: 'Give a short description of your project here.',
-    url: 'https://reactresume.com',
+    url: 'https://github.com/Aziz-fer',
     image: porfolioImage6,
-  },
-  {
-    title: 'Project title 7',
-    description: 'Give a short description of your project here.',
-    url: 'https://reactresume.com',
-    image: porfolioImage7,
-  },
-  {
-    title: 'Project title 8',
-    description: 'Give a short description of your project here.',
-    url: 'https://reactresume.com',
-    image: porfolioImage8,
-  },
-  {
-    title: 'Project title 9',
-    description: 'Give a short description of your project here.',
-    url: 'https://reactresume.com',
-    image: porfolioImage9,
-  },
-  {
-    title: 'Project title 10',
-    description: 'Give a short description of your project here.',
-    url: 'https://reactresume.com',
-    image: porfolioImage10,
-  },
-  {
-    title: 'Project title 11',
-    description: 'Give a short description of your project here.',
-    url: 'https://reactresume.com',
-    image: porfolioImage11,
   },
 ];
 
@@ -268,39 +279,30 @@ export const portfolioItems: PortfolioItem[] = [
  */
 export const education: TimelineItem[] = [
   {
-    date: 'April 2007',
-    location: 'Clown college',
-    title: 'Masters in Beer tasting',
-    content: <p>Describe your experience at school, what you learned, what useful skills you have acquired etc.</p>,
+    date: 'April 2024',
+    location: 'Technical University Of Dortmund',
+    title: 'BSc. Computer Science',
+    content: <p>I completed my Bachlor Degree in 9 Semesters and got 2.7</p>,
   },
   {
     date: 'March 2003',
-    location: 'School of Business',
-    title: 'What did you study 101',
-    content: <p>Describe your experience at school, what you learned, what useful skills you have acquired etc.</p>,
+    location: 'Elite High School of Sousse',
+    title: 'Scientific Baccalaureate',
+    content: <p>I completed my Baccalaureate with a note 1.7 (17.01/20). My studies where focused on Computer Science, Math, Electrical engineering and Mechanical engineering </p>,
   },
 ];
 
 export const experience: TimelineItem[] = [
   {
-    date: 'March 2010 - Present',
-    location: 'Awesome Development Company',
-    title: 'Senior UX Engineer',
+    date: 'Wilo June 2022 - Present',
+    location: 'Wilo Dortmund',
+    title: 'Working Student Software Tester',
     content: (
       <p>
-        Describe work, special projects, notable achievements, what technologies you have been working with, and
-        anything else that would be useful for an employer to know.
-      </p>
-    ),
-  },
-  {
-    date: 'March 2007 - February 2010',
-    location: 'Garage Startup Studio',
-    title: 'Junior bug fixer',
-    content: (
-      <p>
-        Describe work, special projects, notable achievements, what technologies you have been working with, and
-        anything else that would be useful for an employer to know.
+        Worked with a 5-member team developing tools using Python to enable full Automation testing. We also worked with a team from turkey
+        to test different Pump Softwares.
+        Worked on a Project for 6 Months to develop a Matlab Gui using Wilo large databases from polarion and Excel Sheets to improve Motor efficiency of Wilo Pumps. 
+        I worked on cleaning the data necessary for predictive models, analysing quantitative data and representing them in form of Plots and Diagrams.
       </p>
     ),
   },
@@ -313,19 +315,9 @@ export const testimonial: TestimonialSection = {
   imageSrc: testimonialImage,
   testimonials: [
     {
-      name: 'John Doe',
-      text: 'Use this as an opportunity to promote what it is like to work with you. High value testimonials include ones from current or past co-workers, managers, or from happy clients.',
-      image: 'https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/169.jpg',
-    },
-    {
-      name: 'Jane Doe',
-      text: 'Here you should write some nice things that someone has said about you. Encourage them to be specific and include important details (notes about a project you were on together, impressive quality produced, etc).',
-      image: 'https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/14.jpg',
-    },
-    {
-      name: 'Someone else',
-      text: 'Add several of these, and keep them as fresh as possible, but be sure to focus on quality testimonials with strong highlights of your skills/work ethic.',
-      image: 'https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/69.jpg',
+      name: 'Me',
+      text: 'My work ethic is based on collaboration and clear communication. I take pleasure in teamwork and have the ability to explain complicated concepts to both technical and non-technical stakeholders. I can easily learn new technologies, and I work well under pressure.',
+      
     },
   ],
 };
@@ -335,28 +327,33 @@ export const testimonial: TestimonialSection = {
  */
 
 export const contact: ContactSection = {
-  headerText: 'Get in touch.',
-  description: 'Here is a good spot for a message to your readers to let them know how best to reach out to you.',
+  headerText: 'Get in touch with me.',
+  description: '',
   items: [
     {
       type: ContactType.Email,
-      text: 'reachout@timbaker.me',
-      href: 'mailto:reachout@timbaker.me',
+      text: 'ferianiaziz@gmail.com',
+      href: 'mailto:ferianiaziz@gmail.com',
     },
     {
       type: ContactType.Location,
-      text: 'Victoria BC, Canada',
-      href: 'https://www.google.ca/maps/place/Victoria,+BC/@48.4262362,-123.376775,14z',
+      text: 'Dortmund, Germany',
+      href: 'https://www.google.de/maps/place/Dortmund/@51.4719744,7.4285056,12z/data=!4m6!3m5!1s0x47b91760bff07a11:0x427f28131548750!8m2!3d51.5135872!4d7.4652981!16zL20vMDEzNWsy?entry=ttu',
     },
     {
       type: ContactType.Instagram,
-      text: '@tbakerx',
-      href: 'https://www.instagram.com/tbakerx/',
+      text: '@aziz.feriani',
+      href: 'https://www.instagram.com/azizferiani/',
+    },
+    {
+      type: ContactType.LinkedIn,
+      text: '@Aziz Feriani',
+      href: 'https://www.linkedin.com/in/aziz-feriani-761812226/',
     },
     {
       type: ContactType.Github,
-      text: 'tbakerx',
-      href: 'https://github.com/tbakerx',
+      text: 'Aziz-fer',
+      href: 'https://github.com/Aziz-fer',
     },
   ],
 };
@@ -365,9 +362,7 @@ export const contact: ContactSection = {
  * Social items
  */
 export const socialLinks: Social[] = [
-  {label: 'Github', Icon: GithubIcon, href: 'https://github.com/tbakerx'},
-  {label: 'Stack Overflow', Icon: StackOverflowIcon, href: 'https://stackoverflow.com/users/8553186/tim-baker'},
-  {label: 'LinkedIn', Icon: LinkedInIcon, href: 'https://www.linkedin.com/in/timbakerx/'},
-  {label: 'Instagram', Icon: InstagramIcon, href: 'https://www.instagram.com/reactresume/'},
-  {label: 'Twitter', Icon: TwitterIcon, href: 'https://twitter.com/TimBakerx'},
+  {label: 'Github', Icon: GithubIcon, href: 'https://github.com/Aziz-fer'},
+  {label: 'LinkedIn', Icon: LinkedInIcon, href: 'https://www.linkedin.com/in/aziz-feriani-761812226/'},
+  {label: 'Instagram', Icon: InstagramIcon, href: 'https://www.instagram.com/azizferiani/'},
 ];
